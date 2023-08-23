6aee283db94513b425bcb6e9ee73532eb2578c55
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-08-23 18:48:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sampleexoplayer/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.sampleexoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.okhttp.OkHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.ui.PlayerView
+import okhttp3.OkHttpClient
+
+@UnstableApi
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
