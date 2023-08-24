583522e912837a8d81e6a18a10cfa6e999d45f7e
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-08-24 15:52:28 +0530

    fix: Release player when activity close

--- app/src/main/java/com/example/sampleexoplayer/MainActivity.kt
+++ app/src/main/java/com/example/sampleexoplayer/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.sampleexoplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.audio.AudioAttributes
@@ -16,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     val AES_ENCRYPTED_VIDEO_URL = "https://d384padtbeqfgy.cloudfront.net/transcoded_private/B66mmRm2TPF/video.m3u8?Expires=1692809424&Signature=wJ6JZVQHbBEwyhTFy4uYOjK1DhDE9-MyzSO7AD2KnZ1emQxhbYBjSX9tyThG9ZqdMPzec1BKK5lj7LjTp5q9UrYu~VrtyhGDdLwHrQrWUHGE2VUzfokhJ73L8decpDFF3pS0gxHhfgnG~js3WHXORmFfmrSghmr50AOeSHQwyqTyK1eSrV~ufXe5sRQXXLltjYtXyXDWmE02fi1RsWiTvKmXkGVF66hAxFPFR3rGtuqYTC9TA-NYHJacL9HVqIgHLUKp67yXOyjqdSpYIZJaeCh3rk9U2liss7wv-U6pQ4fxCDf3TGOR3k-DG5smU1DEFjHhkCq-U~2wGADT0zGoJQ__&Key-Pair-Id=K2XWKDWM065EGO"
     val access_token = "79ef7b66-bcac-4129-bfe1-34ca3006e799"
+    var exoPlayer : ExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +24,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareExoplayer() {
-        val exoPlayer = ExoPlayer.Builder(this)
+        exoPlayer = ExoPlayer.Builder(this)
             .build()
             .also { exoPlayer ->
                 exoPlayer.setAudioAttributes(AudioAttributes.DEFAULT, true)
                 findViewById<PlayerView>(R.id.player_view).player = exoPlayer
             }
-        exoPlayer.setMediaSource(getMediaSourceFactory().createMediaSource(getMediaItem(AES_ENCRYPTED_VIDEO_URL)))
-        exoPlayer.prepare()
+        exoPlayer?.setMediaSource(getMediaSourceFactory().createMediaSource(getMediaItem(AES_ENCRYPTED_VIDEO_URL)))
+        exoPlayer?.prepare()
     }
 
     private fun getMediaSourceFactory(): MediaSource.Factory {
@@ -54,5 +54,13 @@ class MainActivity : AppCompatActivity() {
     private fun getMediaItem(sourseUrl: String): MediaItem {
         return MediaItem.Builder()
             .setUri(sourseUrl).build()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (exoPlayer != null) {
+            exoPlayer?.release()
+            exoPlayer = null
+        }
     }
 }
